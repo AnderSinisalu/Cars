2bd4a3569f097b2d7e7c202a4cf73d5580829c3e
DESKTOP-DIUE87M\HOME <Ander.sinisalu@gmail.com>
2024-12-16 15:09:15 +0200

    Cars Controller, Views, Applicationservices, Core and Data setup

---
+++ Cars.ApplicationService/Services/CarsServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.ApplicationService.Services
+{
+    internal class CarsServices
+    {
+    }
+}
---
+++ Cars/Models/Car/CarsIndexViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Cars.Models.Car
+{
+    public class CarsIndexViewModel
+    {
+    }
+}
