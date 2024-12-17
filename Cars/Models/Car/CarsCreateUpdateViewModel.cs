51da51b54f716c56e418d3720908a81aa275eab1
DESKTOP-DIUE87M\HOME <Ander.sinisalu@gmail.com>
2024-12-17 09:06:39 +0200

    Add Cars viewmodels and fixed dependencies

--- Cars/Models/Car/CarsCreateUpdateViewModel.cs
+++ Cars/Models/Car/CarsCreateUpdateViewModel.cs
@@ -2,5 +2,22 @@
 {
     public class CarsCreateUpdateViewModel
     {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public string Power { get; set; }
+        public string Fuel { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+    public enum FuelType
+    {
+        Petrol,
+        Diesel,
+        Hybrid,
+        Electric
     }
 }
--- Cars/Models/Car/CarsIndexViewModel.cs
+++ Cars/Models/Car/CarsIndexViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class CarsIndexViewModel
     {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Fuel { get; set; }
     }
 }
